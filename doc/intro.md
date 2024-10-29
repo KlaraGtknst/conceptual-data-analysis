a4dd6b679ba233f848d86769196906cb849e55df
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2024-10-29 15:20:02 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to conceptual-data-analysis
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/conceptual_data_analysis/core_test.clj
@@ -0,0 +1,7 @@
+(ns conceptual-data-analysis.core-test
+  (:require [clojure.test :refer :all]
+            [conceptual-data-analysis.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
